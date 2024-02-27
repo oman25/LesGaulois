11325dd9e2245caa22fea69b7e47532817d2f4d7
oman25 <160142750+oman25@users.noreply.github.com>
2024-02-27 19:54:46 +0100

    Add files via upload

---
+++ LesGaulois/src/personnage/Romain.java
@@ -0,0 +1,27 @@
+package personnage;
+
+public class Romain {
+	private String nom;
+	private int force;
+	public Romain(String nom, int force) {
+	this.nom = nom;
+	this.force = force;
+	}
+	public String getNom() {
+	return nom;
+	}
+	public void parler(String texte) {
+	    System.out.println(prendreParole() + "« " + texte + "»");
+	}
+	
+	public void recevoirCoup(int forceCoup) {
+		force -= forceCoup;
+		if (force > 0) {
+		parler("Aïe");
+		} else {
+		    parler("J'abandonne...");
+		}
+	}
+}
+
+                                                             
---
+++ LesGaulois/src/personnage/chef.java
@@ -0,0 +1,29 @@
+package personnage;
+
+public class chef {
+	private String nom;
+	private int force;
+	private Village village;
+	
+	public Chef(String nom, int force, Village village) {
+		this.nom = nom;
+		this.force = force;
+		this.village = village;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+    }
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+		}
+		private String prendreParole() {
+		return "Le chef " + nom + " du village " + village.getNom() + " : ";
+		}
+		public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " +
+
