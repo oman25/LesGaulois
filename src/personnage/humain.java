610b5127e65257b37be2a5c62e9d9e808edf59d6
oman25 <160142750+oman25@users.noreply.github.com>
2024-03-26 19:52:25 +0100

    Add files via upload

---
+++ src/personnage/humain.java
@@ -0,0 +1,30 @@
+package personnage;
+
+public class humain {
+	private String nom;
+	private String Boissonfavorite;
+	private int argent;
+	
+	public humain(String nom,String Boissonfavorite,int argent) {
+		this.nom = nom;
+		this.Boissonfavorite = Boissonfavorite;
+		this.argent = argent;
+	}
+    public String getnom() {
+    	return nom;
+    }
+    public String getBoissonfavorite() {
+    	return Boissonfavorite;
+    }
+    public int getargent() {
+    	return argent;
+    }
+    private String PrendreParole() {
+    	return nom + " : ";
+    }
+    public void parler(String texte) {
+    	System.out.println(){
+    		
+    	}
+    }
+    
